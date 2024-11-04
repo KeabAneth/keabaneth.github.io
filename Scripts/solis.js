bc0af8375cf1be8e36da49142bcf2725d41a90a6
Keab <137433322+KeabAneth@users.noreply.github.com>
2024-11-04 07:48:49 -0600

    a

--- Scripts/solis.js
+++ Scripts/solis.js
@@ -1,5 +1,5 @@
 const search = document.getElementById("search");
-
+let oofa = document.querySelector(".labels");
 function showContent(tab) {
   document.getElementById('weatherContent').style.display = 'none';
   document.getElementById('historyContent').style.display = 'none';
@@ -18,11 +18,22 @@ document.getElementById("keab").addEventListener("click", () => {
   window.location.href = "https://keabaneth.github.io"
 })
 
-
 search.addEventListener("submit", e => {
   e.preventDefault()
 
 })
+function getDayOfWeek() {
+  const now = new Date;
+  const today = now.getDay();
+  return today;
+}
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let oofIt = 1;
+ for(const oofs of document.getElementById("week").querySelectorAll(".labels")) {
+  oofs.textContent = daysOfWeek[getDayOfWeek() + oofIt]
+  oofIt++;
+ }
 
 function searchLoc() {
   
@@ -48,9 +59,54 @@ script.onload = function () { eruda.init()}
              let final = await response.json()
              forecast(final);
           } catch (error) {
-            // document.getElementById("wind").textContent = "1" + error;
+            document.getElementById("wind").textContent = "1" + error;
           }
  }
+function putWeek() {
+  let i = 1;
+  for (const elements of document.getElementById("week").children) {
+    i++;
+
+    let i2 = 1;
+    for(const el of elements.children) {
+      if(el.classList.contains("labels")) {
+        continue;
+      }
+      let periodOf = i*2+i2-3;
+      for(const days of el.children) {
+        switch (days.getAttribute("class").split(" ")[0]) {
+          case "dayPrec":
+            days.textContent = final2.properties.periods[periodOf].probabilityOfPrecipitation.value + "%";
+            break
+        case "dayForecast":
+            days.textContent = final2.properties.periods[periodOf].shortForecast;
+            break;
+        case "dayTemp":
+            days.textContent = final2.properties.periods[periodOf].temperature + " F";
+            break;
+        case "dayWind":
+            days.textContent =`${final2.properties.periods[periodOf].windDirection} \n${final2.properties.periods[periodOf].windSpeed}`;
+            break;
+            case "nighPrec":
+            days.textContent = final2.properties.periods[periodOf+1].probabilityOfPrecipitation.value + "%";
+            break
+        case "nighForecast":
+            days.textContent = final2.properties.periods[periodOf+1].shortForecast;
+            break;
+        case "nighTemp":
+            days.textContent = final2.properties.periods[periodOf+1].temperature + " F";
+            break;
+        case "nighWind":
+            days.textContent =`${final2.properties.periods[periodOf+1].windDirection} \n${final2.properties.periods[periodOf].windSpeed}`;
+            break;
+            
+        }
+      }
+    }
+ }
+}
+
+
  let response2;
  let final2;
  async function forecast(url2) {
@@ -60,7 +116,7 @@ script.onload = function () { eruda.init()}
              final2 = await response2.json();
              updForecast(final2);
           } catch (error2) {
-            // document.getElementById("wind").textContent = error2 + url2;
+            document.getElementById("wind").textContent = error2 + url2;
           }
   }
   function updForecast(fore) {
@@ -77,4 +133,5 @@ script.onload = function () { eruda.init()}
     document.getElementById("precTon").textContent = final2.properties.periods[1].probabilityOfPrecipitation.value + "%"
     document.getElementById("titleLat").textContent = final2.properties.periods[1].name;
     document.getElementById("titleNow").textContent = final2.properties.periods[0].name;
+    putWeek();
   }
