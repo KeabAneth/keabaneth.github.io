8c144c3bb20b1e043b2403b7a7ce706e6c899bbc
Keab <137433322+KeabAneth@users.noreply.github.com>
2024-11-05 13:19:11 -0600

    a

--- Scripts/linkers.js
+++ Scripts/linkers.js
@@ -14,8 +14,12 @@ const projects = document.querySelectorAll(".projects img");
 for (const proj of projects) {
     proj.addEventListener("mouseenter", e => {
         proj.nextElementSibling.style.display = "block";
+        proj.style.borderBottomLeftRadius = "0px"
+        proj.style.borderBottomRightRadius = "0px"
     })
     proj.addEventListener("mouseleave", e => {
         proj.nextElementSibling.style.display = "none";
+        proj.style.borderBottomLeftRadius = "20px"
+        proj.style.borderBottomRightRadius = "20px"
     })
 }
